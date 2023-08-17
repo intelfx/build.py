b113deea9d1bcd8a36a2b8a95d7ed6b40bb65542
Ivan Shapovalov <intelfx@intelfx.name>
2023-08-18 00:46:15 +0400

    src: buildpy.config: monkey-patch and use @attr.s
    I just cannot withstand the temptation.

--- src/buildpy/config.py
+++ src/buildpy/config.py
@@ -1,12 +1,13 @@
-import attrs
 from pathlib import Path
+import attr, attrs
+attr.s, attr.ib = attrs.define, attrs.field
 
 
 # TODO: GlobalConfig -> Config, where
 #   GlobalConfig: main config file location etc.
 #   Config: per-repo-settings
 
-@attrs.define
+@attr.s
 class Config:
 	config_root: Path = Path.home()/'build.py'
 	pkgbuild_root: Path = Path.home()/'pkgbuild'
