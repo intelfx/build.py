0b41da5eebd4ce73c0f9dea81cb665892ef59f78
Ivan Shapovalov <intelfx@intelfx.name>
2023-08-19 19:19:11 +0400

    src: buildpy.util: pacman_conf_prepend_repo(): return the NamedTemporaryFile
    Rather than wrapping the NamedTemporaryFile in a proper contextmanager, merely
    forward the return value. Specifically, NamedTemporaryFile.__enter__() is a
    no-op and the file can be written before entering.

--- src/buildpy/util.py
+++ src/buildpy/util.py
@@ -32,25 +32,24 @@ def pacman_conf_prepend_repo2(pacman_conf: Path, repo_name: str, repo_section: s
 		yield f
 
 
-@contextlib.contextmanager
 def pacman_conf_prepend_repo(pacman_conf: Path, repo_name: str, repo_section: str) -> typing.TextIO:
 	with pacman_conf.open('r') as f:
 		text = f.read()
 
-	repo_text = f"[{repo_name}]\n{repo_section}"
+	repo_text = f'[{repo_name}]\n{repo_section}'
 
 	if re.search(fr'^\[{repo_name}\]$', text, flags=re.MULTILINE):
-		raise RuntimeError(f"pacman.conf template {pacman_conf} already contains [{repo_name}]")
+		raise RuntimeError(f'pacman.conf template {pacman_conf} already contains [{repo_name}]')
 
 	for m in re.finditer(r'^#?\[(.+)\]$', text, flags=re.MULTILINE):
-		if m.group(1) != "options":
+		if m.group(1) != 'options':
 			prefix, suffix = text[:m.start()], text[m.start():]
-			text = f"{prefix}{util.with_newline(repo_text)}\n{suffix}"
+			text = f'{prefix}{util.with_newline(repo_text)}\n{suffix}'
 			break
 	else:
-		text = f"{util.with_newline(text)}\n{repo_text}"
+		text = f'{util.with_newline(text)}\n{repo_text}'
 
-	with tempfile.NamedTemporaryFile(mode="w+", prefix='pacman', suffix='.conf') as f:
-		f.write(text)
-		f.seek(0)
-		yield f
+	f = tempfile.NamedTemporaryFile(mode='w+', prefix='pacman', suffix='.conf')
+	f.write(text)
+	f.seek(0)
+	return f
