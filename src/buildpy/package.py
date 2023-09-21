8f19d91eda17cd6206ed8f2ecdf6c9d19d88f5de
Ivan Shapovalov <intelfx@intelfx.name>
2023-09-21 23:26:15 +0400

    src: buildpy.package: add classes representing a pkgbase and a pkgname

---
+++ src/buildpy/package.py
@@ -0,0 +1,25 @@
+from typing import (
+	Any,
+)
+
+import attr, attrs
+attr.s, attr.ib = attrs.define, attrs.field
+
+
+@attr.s
+class Pkgbase:
+	pkgbase: str
+	pkgnames: 'list[Pkgname]'
+	depends: 'list[Pkgname|str]'
+	makedepends: 'list[Pkgname|str]'
+	optdepends: 'list[Pkgname|str]'
+	provider: Any
+
+
+@attr.s
+class Pkgname:
+	pkgbase: Pkgbase
+	pkgname: str
+	depends: 'list[Pkgname|str]'
+	makedepends: 'list[Pkgname|str]'
+	optdepends: 'list[Pkgname|str]'
