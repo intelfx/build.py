9e707b64274f678da4d4157886fb7f3625e5f5f2
Ivan Shapovalov <intelfx@intelfx.name>
2023-10-07 21:55:05 +0400

    src: buildpy.context: add privilege elevation wrappers (fake and real)

--- src/buildpy/config.py
+++ src/buildpy/config.py
@@ -1,3 +1,4 @@
+import enum
 from pathlib import Path
 import attr, attrs
 attr.s, attr.ib = attrs.define, attrs.field
@@ -7,6 +8,12 @@ attr.s, attr.ib = attrs.define, attrs.field
 #   GlobalConfig: main config file location etc.
 #   Config: per-repo-settings
 
+class Auth(enum.Enum):
+	No = enum.auto()
+	Fake = enum.auto()
+	Real = enum.auto()
+
+
 @attr.s
 class Config:
 	config_root: Path = Path('/etc/aurutils')
@@ -15,3 +22,14 @@ class Config:
 	repo_name: str = 'custom'
 	makepkg_conf: Path = config_root/f'makepkg-{repo_name}.conf'
 	pacman_conf: Path = config_root/f'pacman-{repo_name}.conf'
+
+	auth_wrappers: dict[Auth, list[str]] = {
+		Auth.Fake: [ 'unshare', '-Ur' ],
+		Auth.Real: [ 'sudo' ],
+	}
+
+	def auth_wrapper(self, auth: Auth) -> list[str]:
+		return self.auth_wrappers.get(auth, [])
+
+	def with_auth_wrapper(self, auth: Auth, cmd: list[str]) -> list[str]:
+		return self.auth_wrapper(auth) + cmd
